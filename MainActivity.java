b253cab8bb20f5b9faf875eac3ed1ecb2940118a
why114514ssg-boop <why114514ssg@gmail.com>
2026-03-31 04:26:56 +0800

    Create MainActivity.java

---
+++ MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.soccermanager.ui;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.soccermanager.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+    }
+}
